5a9d8bddca7c228f908796d950409158b4bb861f
Sergey Galuzin <galserg@gmail.com>
2022-05-11 15:46:51 +0300

    release

--- src/argo-helmfile.sh
+++ src/argo-helmfile.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 ## specially handled ENV vars
-# HELM_BINARY - custom path to helm binary
-# HELM_TEMPLATE_OPTIONS - helm template --help
+
 # HELMFILE_BINARY - custom path to helmfile binary
 # HELMFILE_GLOBAL_OPTIONS - helmfile --help
 # HELMFILE_TEMPLATE_OPTIONS - helmfile template --help
-# HELMFILE_HELMFILE - a complete helmfile.yaml (ignores standard helmfile.yaml and helmfile.d if present based on strategy)
-# HELMFILE_HELMFILE_STRATEGY - REPLACE or INCLUDE
-# HELMFILE_INIT_SCRIPT_FILE - path to script to execute during the init phase
-# HELM_DATA_HOME - perform variable expansion
-
-# NOTE: only 1 -f value/file/dir is used by helmfile, while you can specific -f multiple times
-# only the last one matters and all previous -f arguments are irrelevant
-
-# NOTE: helmfile pukes if both helmfile.yaml and helmfile.d are present (and -f isn't explicity used)
+# HELMFILE_FILE - file for deploy (-f options)
+# HELMFILE_ENV - env (-e options)
 
 ## standard build environment
 ## https://argoproj.github.io/argo-cd/user-guide/build-environment/
@@ -24,10 +16,6 @@
 # ARGOCD_APP_SOURCE_PATH - the path of the app within the repo
 # ARGOCD_APP_SOURCE_REPO_URL the repo's URL
 # ARGOCD_APP_SOURCE_TARGET_REVISION - the target revision from the spec, e.g. master.
-
-# each manifest generation cycle calls git reset/clean before (between init and generate it is NOT ran)
-# init is called before every manifest generation
-# it can be used to download dependencies, etc, etc
 
 # does not have "v" in front
 # KUBE_VERSION="<major>.<minor>"
@@ -55,7 +43,7 @@ else
   else
     LOCAL_HELMFILE_BINARY="/tmp/__${SCRIPT_NAME}__/bin/helmfile"
     if [[ ! -x "${LOCAL_HELMFILE_BINARY}" ]]; then
-      wget -O "${LOCAL_HELMFILE_BINARY}" "https://github.com/roboll/helmfile/releases/download/v0.138.7/helmfile_linux_amd64"
+      wget -O "${LOCAL_HELMFILE_BINARY}" "https://github.com/roboll/helmfile/releases/download/v0.144.0/helmfile_linux_amd64"
       chmod +x "${LOCAL_HELMFILE_BINARY}"
     fi
     helmfile="${LOCAL_HELMFILE_BINARY}"
@@ -70,12 +58,18 @@ if [[ "${HELMFILE_GLOBAL_OPTIONS}" ]]; then
   helmfile="${helmfile} ${HELMFILE_GLOBAL_OPTIONS}"
 fi
 
+if [[ "${HELMFILE_ENV}" ]]; then
+  helmfile="${helmfile} --environment ${HELMFILE_ENV}"
+fi
+
+if [[ "${HELMFILE_FILE}" ]]; then
+  helmfile="${helmfile} --file ${HELMFILE_FILE}"
+fi
+
 echoerr "$(${helmfile} --version)"
-echoerr "$(env | grep ARGOCD_)"
 
-
-${helmfile} -e prod -f producer.yaml \
+${helmfile} \
   template \
-  --skip-deps ${INTERNAL_HELMFILE_TEMPLATE_OPTIONS} \
+  --skip-deps \
   ${HELMFILE_TEMPLATE_OPTIONS}
 
